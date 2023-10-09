62ab6fe932a45e9b7556c641f43e0231753fd13b
Chattercube <33176933+Chattercube@users.noreply.github.com>
2023-10-09 22:24:39 +0800

    First

---
+++ js/router.js
@@ -0,0 +1,26 @@
+const route = (event) => {
+    event = event || window.event;
+    event.preventDefault();
+    window.history.pushState({}, "", event.target.href);
+    handleLocation();
+  };
+  
+const routes = {
+    404: "/pages/home/home.html",
+    "/": "/pages/home/home.html",
+    "/home": "/pages/home/home.html",
+    "/test2": "/pages/test2/test2.html"
+};
+  
+const handleLocation = async () => {
+    const path = window.location.pathname;
+    const route = routes[path] || routes[404];
+    const html = await fetch(route).then((data) => data.text());
+    document.querySelector("#main-page").innerHTML = html;
+};
+  
+window.onpopstate = handleLocation;
+window.route = route;
+  
+handleLocation();
+  
